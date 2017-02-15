74a4dfe258bf1b230d943a5acab9767895b1835c
marcelamelobh <marcelaamelo1986@gmail.com>
2017-02-14 23:07:49 -0200

    solved problem with routers - htaccess inserted router update term

--- src/controllers/term.php
+++ src/controllers/term.php
@@ -21,8 +21,8 @@ $app->post('/term/save', function () use($app){
         $term->version_name = $app->request()->post('version_name');
         $term->title = $app->request()->post('title');
         $term->content = $app->request()->post('content');
-        $term->publiction_date = $app->request()->post('publication_date');
-        $term->is_active = $app->request()->post('Y');
+        $term->publication_date = $app->request()->post('publication_date');
+        $term->is_active = 'Y';
         
         if($term->save()){
             $data = term::find($term->id);
@@ -31,6 +31,25 @@ $app->post('/term/save', function () use($app){
         
     } catch (Exception $ex) {
         $error = new custonError(2, $ex->getCode(), $ex->getMessage());
+        $data = $error->parse_error();
+        return helpers::jsonResponse($data);
+    }
+});
+
+$app->put('/term/:id', function ($id) use($app){
+    try{
+        $term = term::find($id);
+        $term->version_name = $app->request()->params('version_name');
+        $term->title = $app->request()->params('title');
+        $term->content = $app->request()->params('content');
+        $term->publication_date = $app->request()->params('publication_date');
+        $term->is_active = $app->request()->params('is_active');
+
+        if($term->update()){
+            return helpers::jsonResponse($term);
+        } 
+    } catch (Exception $ex) {
+        $error = new custonError(4, $ex->getCode(), $ex->getMessage());
         $data = $error->parse_error();
         return helpers::jsonResponse($data);
     }
