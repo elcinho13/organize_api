b1527cf4223ec06ae16ee075c24e5a2bfb6d2612
marcelamelobh <marcelaamelo1986@gmail.com>
2017-02-19 15:23:08 -0300

    alter routers user security

--- src/models/user_security.php
+++ src/models/user_security.php
@@ -6,11 +6,11 @@ class user_security extends Eloquent{
     public $timestamps = true;
     protected $table = 'org_user_security';
     
-    public function org_user_id(){
-        return $this->belongsTo('user','org_user_id');
+    public function user(){
+        return $this->belongsTo('user','user');
     }
     
-    public function org_security_question_id(){
-        return $this->belongsTo('security_question','org_security_question_id');
+    public function security_question(){
+        return $this->belongsTo('security_question','security_question');
     }
 }
