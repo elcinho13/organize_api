65f269df81aaa07e5a328233b37ffff1d99e21d2
marcelamelobh <marcelaamelo1986@gmail.com>
2017-03-13 23:18:12 -0300

    alter routers user

---
+++ src/controllers/user_type.php
@@ -0,0 +1,43 @@
+<?php
+
+$app->get('/user_type/:locale', function ($locale) {
+    try {
+        $data = user_type::query()->where('locale', '=', $locale)->get();
+        return helpers::jsonResponse($data);
+    } catch (Exception $ex) {
+        $error = new custonError(3, $ex->getCode(), $ex->getMessage());
+        return helpers::jsonResponse($error->parse_error());
+    }
+});
+
+$app->get('/user_type/:locale/:id', function ($locale, $id) {
+    try {
+        $user_types = user_type::query()->where('locale', '=', $locale)->get();
+        foreach ($user_types as $user_type) {
+            if ($user_type->code_enum == $id) {
+                $data = $user_type;
+            }
+        }
+        return helpers::jsonResponse($data);
+    } catch (Exception $ex) {
+        $error = new custonError(3, $ex->getCode(), $ex->getMessage());
+        return helpers::jsonResponse($error->parse_error());
+    }
+});
+
+$app->post('/user_type/save', function () use($app) {
+    try {
+        $user_type = new user_type();
+        $user_type->locale = $app->request()->post('locale');
+        $user_type->code_enum = $app->request()->post('code_enum');
+        $user_type->name = $app->request()->post('name');
+
+        if ($user_type->save()) {
+            $data = user_type::find($user_type->id);
+            return helpers::jsonResponse($data);
+        }
+    } catch (Exception $ex) {
+        $error = new custonError(2, $ex->getCode(), $ex->getMessage());
+        return helpers::jsonResponse($error->parse_error());
+    }
+});
---
+++ src/models/user_type.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Database\Eloquent\Model as Eloquent;
+
+class login_type extends Eloquent {
+
+    public $timestamps = true;
+    protected $table = 'org_login_type';
+
+}
