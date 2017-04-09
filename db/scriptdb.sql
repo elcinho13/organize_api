24cdb04547dd38b976e9c521520ed209b34f9202
marcelamelobh <marcelaamelo1986@gmail.com>
2017-04-09 17:06:02 -0300

    alter script database for modify first access and user

--- db/scriptdb.sql
+++ db/scriptdb.sql
@@ -198,12 +198,12 @@ CREATE TABLE IF NOT EXISTS `org_first_access`(
 CREATE TABLE IF NOT EXISTS `org_user`(
     `id` INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
     `user_type` INT UNSIGNED,
+    `first_access` INT UNSIGNED,
+    `token` INT UNSIGNED,
+    `plan` INT UNSIGNED,
     `full_name` VARCHAR(255),
     `mail` VARCHAR(255) UNIQUE,
     `password` VARCHAR(255),
-    `facebook_id` VARCHAR(255),
-    `linkedin_id` VARCHAR(255),
-    `google_id` VARCHAR(255),
     `profile_picture` VARCHAR(255),
     `cpf` VARCHAR(255) UNIQUE,
     `rg_number` VARCHAR(255),
@@ -214,18 +214,38 @@ CREATE TABLE IF NOT EXISTS `org_user`(
     `gender` VARCHAR(1),
     `responsible_name` VARCHAR(255),
     `responsible_cpf` VARCHAR(255),
-    `term` INT UNSIGNED, 
-    `term_accept` TINYINT(1), 
-    `term_accept_date` TIMESTAMP,
-    `plan` INT,
     `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `updated_at` TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP,
 
     KEY `org_user_fk1` (`user_type`),
     CONSTRAINT `org_user_fk1` FOREIGN KEY (`user_type`) REFERENCES `org_user_type` (`id`),
 
-    KEY `org_user_fk2` (`term`),
-    CONSTRAINT `org_user_fk2` FOREIGN KEY (`term`) REFERENCES `org_term` (`id`)
+    KEY `org_user_fk2` (`first_access`),
+    CONSTRAINT `org_user_fk2` FOREIGN KEY (`first_access`) REFERENCES `org_first_access` (`id`),
+
+    KEY `org_user_fk3` (`token`),
+    CONSTRAINT `org_user_fk3` FOREIGN KEY (`token`) REFERENCES `org_token` (`id`),
+
+    KEY `org_user_fk4` (`plan`),
+    CONSTRAINT `org_user_fk4` FOREIGN KEY (`plan`) REFERENCES `org_plan` (`id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- -----------------------------------------------------------------------------
+-- Create table org_user_term
+-- -----------------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS `org_user_term`(
+    `id` INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    `user` INT UNSIGNED,
+    `term` INT UNSIGNED, 
+    `term_accept` TINYINT(1), 
+    `term_accept_date` TIMESTAMP,
+
+    KEY `org_user_term_fk1` (`user`),
+    CONSTRAINT `org_user_term_fk1` FOREIGN KEY (`user`) REFERENCES `org_user` (`id`),
+
+    KEY `org_user_term_fk2` (`term`),
+    CONSTRAINT `org_user_term_fk2` FOREIGN KEY (`term`) REFERENCES `org_term` (`id`)
+
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- -----------------------------------------------------------------------------
@@ -273,24 +293,19 @@ CREATE TABLE IF NOT EXISTS `org_user_security`(
 -- -----------------------------------------------------------------------------
 CREATE TABLE IF NOT EXISTS `org_token`(
     `id` INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    `user` INT UNSIGNED,
-    `first_access` INT UNSIGNED,
     `login_type` INT UNSIGNED,
     `access_token` VARCHAR(255),
+    `facebook_id` VARCHAR(255),
+    `linkedin_id` VARCHAR(255),
+    `google_id` VARCHAR(255),
     `access_platform` INT,
     `access_date` TIMESTAMP,
     `keep_logged` TINYINT(1),
     `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `updated_at` TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP,
 
-    KEY `org_token_fk1` (`user`),
-    CONSTRAINT `org_token_fk1` FOREIGN KEY (`user`) REFERENCES `org_user` (`id`),
-
-    KEY `org_token_fk2` (`first_access`),
-    CONSTRAINT `org_token_fk2` FOREIGN KEY (`first_access`) REFERENCES `org_first_access` (`id`),
-
-    KEY `org_token_fk3` (`login_type`),
-    CONSTRAINT `org_token_fk3` FOREIGN KEY (`login_type`) REFERENCES `org_login_type` (`id`)
+    KEY `org_token_fk1` (`login_type`),
+    CONSTRAINT `org_token_fk1` FOREIGN KEY (`login_type`) REFERENCES `org_login_type` (`id`)
 
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
