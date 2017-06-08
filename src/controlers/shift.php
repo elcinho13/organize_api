5e2b7c7667f4a3df425abad75b9f7ff0d9d75cc5
Elcio <elcinho.13@gmail.com>
2017-06-08 03:26:50 -0300

    commit sprint 3 api

---
+++ src/controlers/shift.php
@@ -0,0 +1,63 @@
+<?php
+
+$app->get('/shift/:locale', function($locale){
+	try {
+		$data = shift::query()->where('locale', '=', $locale)->get();
+		$error = new custonError(false, 0);
+		return helpers::jsonResponse($error->parse_error(), $data);
+	}catch (Exception $ex){
+		$error = new custonError(true, 2 , $ex->getCode(), $ex->getMessage());
+		return helpers::jsonResponse($error->parse_error(), null);
+	}
+});
+
+
+$app->get('/shift/:locale/:id', function ($locale, $id){
+	try{
+		$data = shift::query()->where('locale','=',$locale)->where('code_enum','=',$id)->get();
+		$error = new custonError(false,0);
+		return helpers::jsonResponse($error->parse_error(), $data);
+	}catch(Exception $ex){
+		$error = new custonError(true, 2, $ex->getCode(), $ex->getMessage());
+		return helpers::jsonResponse($error->parse_error(), null);
+	}
+});
+
+
+$app->post('/shift/save', function() use($app){
+	try{
+		$shift = new shift();
+		$shift->locale = $app->request()->post('locale');
+		$shift->locale = $app->request()->post('locale');
+		$shift->code_enum = $app->request()->post('code_enum');
+		$shift->name = $app->request->post('name');
+		$shift->user_last_update = $app->request()->post('user_admin');
+
+		if($shift->save()){
+			$data = shift::find($shift->id);
+			$error = new custonError(false, 0);
+			return helpers::jsonResponse($error->parse_error(), $data);
+		}
+	}catch (Exception $ex) {
+		$error = new custonError(true, 3, $ex->getCode(), $ex->getMessage());
+		return helpers::jsonResponse($error->parse_error(),null);
+	}
+});
+
+
+$app->post('/shift/:id/active', function ($id) use($app) {
+    try {
+        $shift = shift::find($id);
+        $shift->is_active = $app->request()->post('is_active');
+
+        if ($shift->update()) {
+            $data = shift::find($shift->id);
+            $error = new custonError(false, 0);
+            return helpers::jsonResponse($error->parse_error(), $data);
+        }
+    } catch (Exception $ex) {
+        $error = new custonError(true, 4, $ex->getCode(), $ex->getMessage());
+        return helpers::jsonResponse($error->parse_error(), null);
+    }
+});
+
---
+++ src/controlers/user_social_network.php
@@ -0,0 +1,69 @@
+<?php
+
+
+$app->get('/user_social_network/user/:user', function($user_id) {
+	try{
+		$data = user_social_network::where('user', '=', $user_id)->get();
+		$error = new custonError(false, 0);
+		return helpers::jsonResponse($error->parse_error(), $data);
+	}catch (Exception $ex){
+		$error = new custonError(true, 2, $ex->getCode(), $ex->getMessage());
+		return helpers::jsonResponse($error->parse_error(), null);
+	}
+
+});
+
+
+
+$app->get('/user_social_network/:id', function($id) {
+
+	try{
+        $data = user_social_network::find($id);
+		$error = new custonError(false, 0);
+		return helpers::jsonResponse($error->parse_error(), $data);
+	}catch (Exception $ex){
+		$error = new custonError(true, 2, $ex->getCode(),$ex->getMessage());
+		return helpers::jsonResponse($error->parse_error(), null);
+	}
+});
+
+
+
+$app->post('/user_social_network/save', function() use($app){
+	try{
+		$user_social_network = new user_social_network();
+		$user_social_network->social_network_type = $app->request()->post('social_network_type');
+		$user_social_network->user = $app->request()->post('user');
+		$user_social_network->url = $app->request()->post('url');
+		$user_social_network->user_last_update = $app->request()->post('user_admin');
+
+		if($user_social_network->save()){
+			$data = user_social_network::with(relations::getSocialNetworkRelations())->find($user_social_network->id);
+			$error = new custonError(false, 0);
+			return helpers::jsonResponse($error->parse_error(), $data);
+		}
+	}catch (Exception $ex) {
+		$error = new custonError(true, 3, $ex->getCode(), $ex->getMessage());
+		return helpers::jsonResponse($error->parse_error(), null);
+	}
+
+
+});
+
+
+$app->post('/user_social_network/:id/active', function ($id) use($app) {
+    try {
+        $user_social_network = user_social_network::find($id);
+        $user_social_network->is_active = $app->request()->post('is_active');
+
+        if ($user_social_network->update()) {
+            $data = user_social_network::with(relations::getSocialNetworkRelations())->find($user_social_network->id);
+            $error = new custonError(false, 0);
+            return helpers::jsonResponse($error->parse_error(), $data);
+        }
+    } catch (Exception $ex) {
+        $error = new custonError(true, 4, $ex->getCode(), $ex->getMessage());
+        return helpers::jsonResponse($error->parse_error(), null);
+    }
+});
+
